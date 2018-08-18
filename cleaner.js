65cab4958c29147c740c7a25f9668eb26de90c94
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2018-08-17 17:00:20 -0700

    update response format

--- cleaner.js
+++ cleaner.js
@@ -2,7 +2,10 @@ module.exports = (rawJSON, context, handler) => {
     let cleaned = rawJSON.filter(e => e.name.length > 0 && 
         e.instructor.length > 0 &&
         e.total.length > 0 &&
-        e.available.length > 0)
+        e.available.length > 0).map(e => {
+            return `Name: ${e.name} \n- ${e.instructor}\n 
+            Available: ${e.available}\nTotal: ${e.total}` 
+        }).map(e => `${e}\n` )
     
     console.log(cleaned)
 }
