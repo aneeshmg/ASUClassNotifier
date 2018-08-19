5fad1875dc247cdd158af9eaa753061d6304e894
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2018-08-18 19:26:21 -0700

    fix responder

--- responder.js
+++ responder.js
@@ -1,7 +1,9 @@
 const axios = require('axios')
 const config = require('./config')
 
-const send = (data, to, chatId) => {
+// send("WOW", "aneeshmg", "b693daf4d84832b7aec6b942b992fdca2a740937b18d5025af9756250928f607")
+
+module.exports = (data, to, chatId) => {
     const url = 'https://api.kik.com/v1/message'
     const auth = {
         user: config.username,
@@ -20,17 +22,10 @@ const send = (data, to, chatId) => {
     const headers = { 
         'Authorization' : `Basic ${Buffer.from(`${config.username}:${config.apiKey}`).toString('base64')}` 
     }
-    console.log(headers)
     axios({
         url: url,
         method: 'post',
         headers: headers,
         data: payload
     }).then(e => console.log(e.data)).catch(e => console.log('FAILURE: ' + e))
-}
-
-send("WOW", "aneeshmg", "b693daf4d84832b7aec6b942b992fdca2a740937b18d5025af9756250928f607")
-
-module.exports = () => {
-    send
 }
